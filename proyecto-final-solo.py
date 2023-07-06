9a856e1eaae1719ffd77735b07edba6e751934f6
nancygcode <nancysofia1998@gmail.com>
2023-07-05 22:20:14 -0600

    uodate

--- proyecto-final-solo.py
+++ proyecto-final-solo.py
@@ -3,3 +3,10 @@
 
 # inicio de codigo
 #menu 
+
+registro_medico = input ("¿desea realizar otro proceso? Si/No:")
+if registro_medico.lower() == "no":
+    print ("Gracias por usar nuestro servicio ^^")
+
+# .lower() para hacer todo un string en minusculo 
+# .upper() para hacer todo un string en mayuscula
